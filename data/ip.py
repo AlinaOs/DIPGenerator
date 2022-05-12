de41556a462fbd5d12ec8b776a0e545af22e0511
AlinaOs <47086948+AlinaOs@users.noreply.github.com>
2022-05-12 11:30:40 +0200

    Basic unpack AIP to tmp directory

--- data/ip.py
+++ data/ip.py
@@ -1,32 +1,38 @@
+import os
 from abc import ABC, abstractmethod
 import tempfile
 import tarfile
 
+delim = "\\"
 
 class AbstractIP(ABC):
 
     def __init__(self, path, temp):
-        pass
+        self.path = path
+        self.ipid = None
+        self.temp = temp
+        self.files = []
+        self.metadata = None
 
-    @property
-    def path(self):
-        return self._path
-
-    @property
-    def temp(self):
-        return self._temp
-
-    @property
-    def ipid(self):
-        return self._ipid
-
-    @property
-    def metadata(self):
-        return self._metadata
-
-    @property
-    def files(self):
-        return self._files
+    # @property
+    # def path(self):
+    #     return self._path
+    #
+    # @property
+    # def temp(self):
+    #     return self.temp
+    #
+    # @property
+    # def ipid(self):
+    #     return self.ipid
+    #
+    # @property
+    # def metadata(self):
+    #     return self.metadata
+    #
+    # @property
+    # def files(self):
+    #     return self.files
 
     def save(self, path):
         pass
@@ -43,17 +49,25 @@ class AbstractIP(ABC):
 
         tar.close()
 
-    @files.setter
-    def files(self, value):
-        self._files = value
-
-    @metadata.setter
-    def metadata(self, value):
-        self._metadata = value
-
-    @ipid.setter
-    def ipid(self, value):
-        self._ipid = value
+    # @path.setter
+    # def path(self, value):
+    #     self._path = value
+    #
+    # @temp.setter
+    # def temp(self, value):
+    #     self._temp = value
+    #
+    # @files.setter
+    # def files(self, value):
+    #     self._files = value
+    #
+    # @metadata.setter
+    # def metadata(self, value):
+    #     self._metadata = value
+    #
+    # @ipid.setter
+    # def ipid(self, value):
+    #     self._ipid = value
 
 
 class AIP(AbstractIP):
@@ -66,19 +80,28 @@ class AIP(AbstractIP):
         self.index = None
         self.ieid = None
         self.date = None
-        self.ipid = None
-        self.initSuccess = None
-        self.listfiles()
-        self.extractmetadata()
+        self.initSuccess = False
+        self.__parse__()
 
-    def listfiles(self):
-        tar = tarfile.open(self.__path__)
+    def __parse__(self):
+        tar = tarfile.open(self.path)
         # Open tar to read
         # Or unpack to temporary directory?
         # https://docs.python.org/3/library/tarfile.html#examples
         files = tar.getmembers()
-
+        for f in files:
+            if f.name == "DIPSARCH.xml":
+                tar.extractall(path=self.temp.name, members=[f])
+                self.metadata = self.temp.name + delim + "DIPSARCH.xml"
+                os.rename(self.temp.name+delim+"DIPSARCH.xml", self.temp.name+delim+"123.xml")
+                print("DIPSARCH found:")
+                self.extractmetadata()
+                self.metadata = self.temp.name + delim + str(self.ipid) + ".xml"
+                print(self.metadata)
+            else:
+                self.files.append(f.name)
         tar.close()
+        print(self.files)
 
     def extractmetadata(self):
         self.ieid = 123
