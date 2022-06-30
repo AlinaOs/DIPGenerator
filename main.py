d47da1e2457d88801b91d50a418b7d704cedaf27
AlinaOs <47086948+AlinaOs@users.noreply.github.com>
2022-06-30 10:21:54 +0200

    Prepare code for bundling

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """Main script starting the Request Viewer GUI and initializing the DIP Request Handler."""
 
+import os
 from drh.drh import DIPRequestHandler
 from rv.rv import RequestViewer
+
+os.environ['SAXONC_HOME'] = os.path.join(os.getcwd(), "saxonpy", "saxonc_home")
 
 confdir = "config/DIP/"
 conf = "profile_conf.json"
